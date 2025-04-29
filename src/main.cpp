2205a059c747ccbb3291894e32c44e985232cda6
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-29 14:45:09 -0500

    correcto usando mitad codigo interrupciones con while

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+/*
+//todo en la interrupcion
 #define F_CPU 16000000UL  
 #include <avr/io.h>  
 #include <util/delay.h> 
@@ -107,5 +109,123 @@ int main(void) {
 
     while (1) {
        
+    }
+}
+
+*/
+
+#define F_CPU 16000000UL  
+#include <avr/io.h>  
+#include <util/delay.h> 
+#include <avr/interrupt.h>
+
+char A = 0;
+char B = 0;
+char C = 0;
+char D = 0;
+char E = 0;
+char F = 0;
+
+void config_PCI(void) {
+    PCICR |= 0x01; // habilita interrupciones en el puerto B
+    PCMSK0 |= 0x01; // B0
+    DDRB &= ~0x01;
+    PORTB |= 0x01;
+
+    PCMSK0 |= 0x02; // B1
+    DDRB &= ~0x02;
+    PORTB |= 0x02;
+
+    PCMSK0 |= 0x04; // B2
+    DDRB &= ~0x04;
+    PORTB |= 0x04;
+}
+
+ISR(PCINT0_vect) {
+    _delay_ms(100); // antirrebote
+
+    if (!(PINB & 0x02) && !(PINB & 0x01)) { // B1 + B0
+        A = 1;
+        B = C = D = E = F = 0;
+    }
+    else if (!(PINB & 0x01) && !(PINB & 0x04)) { // B0 + B2
+        B = 1;
+        A = C = D = E = F = 0;
+    }
+    else if (!(PINB & 0x02) && !(PINB & 0x04)) { // B1 + B2
+        D = 1;
+        A = B = C = E = F = 0;
+    }
+    else if (!(PINB & 0x02)) { // solo B1
+        E = 1;
+        A = B = C = D = F = 0;
+    }
+    else if (!(PINB & 0x01)) { // solo B0
+        F = 1;
+        A = B = C = D = E = 0;
+    }
+    else if (!(PINB & 0x04)) { // solo B2
+        C = 1;
+        A = B = D = E = F = 0;
+    }
+}
+
+int main(void) {
+    DDRD |= 0xC0; // motor1: PD6 y PD7
+    PORTD &= ~0xC0;
+
+    DDRD |= 0x0C; // motor2: PD2 y PD3
+    PORTD &= ~0x0C;
+
+    config_PCI();
+    sei(); // habilita interrupciones globales
+
+    while (1) {
+        if (A == 1) {
+            PORTD &= ~0x0C; // apaga motor2
+            PORTD &= ~0xC0; // apaga motor1
+            _delay_ms(100);
+            PORTD |= 0x80; // motor1 adelante
+            PORTD &= ~0x40; // motor1: solo PD7 activo
+            A = 0;
+        }
+        else if (E == 1) {
+            PORTD &= ~0x0C;
+            PORTD &= ~0xC0;
+            _delay_ms(100);
+            PORTD |= 0x40; // motor1 atrás
+            PORTD |= 0x08; // motor2 adelante
+            E = 0;
+        }
+        else if (B == 1) {
+            PORTD &= ~0x0C;
+            PORTD &= ~0xC0;
+            _delay_ms(100);
+            PORTD |= 0x04; // motor2 atrás
+            PORTD &= ~0x08;
+            B = 0;
+        }
+        else if (F == 1) {
+            PORTD &= ~0x0C;
+            PORTD &= ~0xC0;
+            _delay_ms(100);
+            PORTD |= 0x08; // motor2 adelante
+            PORTD |= 0x80; // motor1 adelante
+            F = 0;
+        }
+        else if (D == 1) {
+            PORTD &= ~0x0C;
+            PORTD &= ~0xC0;
+            _delay_ms(100);
+            PORTD |= 0x80; // motor1 adelante
+            PORTD |= 0x04; // motor2 atrás
+            D = 0;
+        }
+        else if (C == 1) {
+            PORTD &= ~0x0C; // apaga motor2
+            PORTD &= ~0xC0; // apaga motor1
+            _delay_ms(100);
+            C = 0;
+        }
     }
 }
